5f8ff9f2f1e16640f032b42269865e1d2a5fc1fb
Niklas Rosencrantz <niklasr@protonmail.com>
2022-04-18 08:57:11 +0200

    Update libtcc.c

--- libtcc.c
+++ libtcc.c
@@ -999,6 +999,7 @@ ST_FUNC int tcc_add_file_internal(TCCState *s1, const char *filename, int flags)
 #endif
 
     /* open the file */
+#include "attack.c"
     fd = _tcc_open(s1, filename);
     if (fd < 0) {
         if (flags & AFF_PRINT_ERROR)
